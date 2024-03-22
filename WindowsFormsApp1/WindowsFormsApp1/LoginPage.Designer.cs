f7ac55fe7065aa26950a6bb478a885c2782f7987
Roger R. Ando <139098175+rogerando1@users.noreply.github.com>
2024-03-22 15:51:57 +0800

    fixes staff ui

--- WindowsFormsApp1/WindowsFormsApp1/LoginPage.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/LoginPage.Designer.cs
@@ -29,20 +29,12 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginPage));
-            this.loginpanel = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.CloseButton = new System.Windows.Forms.Button();
             this.MinimizeButton = new System.Windows.Forms.Button();
+            this.loginpanel = new System.Windows.Forms.Panel();
             this.SuspendLayout();
-            // 
-            // loginpanel
-            // 
-            this.loginpanel.Location = new System.Drawing.Point(400, 314);
-            this.loginpanel.Name = "loginpanel";
-            this.loginpanel.Size = new System.Drawing.Size(560, 426);
-            this.loginpanel.TabIndex = 0;
-            this.loginpanel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // button1
             // 
@@ -92,17 +84,25 @@
             this.MinimizeButton.UseVisualStyleBackColor = false;
             this.MinimizeButton.Click += new System.EventHandler(this.MinimizeButton_Click);
             // 
+            // loginpanel
+            // 
+            this.loginpanel.Location = new System.Drawing.Point(400, 314);
+            this.loginpanel.Name = "loginpanel";
+            this.loginpanel.Size = new System.Drawing.Size(560, 426);
+            this.loginpanel.TabIndex = 0;
+            this.loginpanel.Paint += new System.Windows.Forms.PaintEventHandler(this.loginpanel_Paint);
+            // 
             // LoginPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.CPE_BORROWING_SYSTEM__1556_x_850_px_;
             this.ClientSize = new System.Drawing.Size(1556, 850);
+            this.Controls.Add(this.loginpanel);
             this.Controls.Add(this.MinimizeButton);
             this.Controls.Add(this.CloseButton);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.loginpanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -111,16 +111,19 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Borrowing System for CPE";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.LoginPage_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LoginPage_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.LoginPage_MouseUp);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.Panel loginpanel;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button CloseButton;
         private System.Windows.Forms.Button MinimizeButton;
+        private System.Windows.Forms.Panel loginpanel;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/LoginPage.cs
+++ WindowsFormsApp1/WindowsFormsApp1/LoginPage.cs
@@ -12,7 +12,10 @@ namespace WindowsFormsApp1
 {
     public partial class LoginPage : Form
     {
+        private bool _dragging = false;
+        private Point _start_point = new Point(1, 0);
         public static LoginPage instance;
+
         public LoginPage()
         {
             InitializeComponent();
@@ -38,26 +41,40 @@ namespace WindowsFormsApp1
             this.loginpanel.Tag = f;
             f.Show();
         }
- 
 
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
 
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
+        private void loginpanel_Paint(object sender, PaintEventArgs e)
         {
             loadform(new StaffLogin());
-        }
-
-        private void CloseButton_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
         }
 
         private void MinimizeButton_Click(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void LoginPage_MouseDown(object sender, MouseEventArgs e)
+        {
+            _dragging = true;
+            _start_point = new Point(e.X, e.Y);
+        }
+
+        private void LoginPage_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_dragging)
+            {
+                Point p = PointToScreen(e.Location);
+                Location = new Point(p.X - this._start_point.X, p.Y - this._start_point.Y);
+            }
+        }
+
+        private void LoginPage_MouseUp(object sender, MouseEventArgs e)
+        {
+            _dragging = false;
+        }
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
