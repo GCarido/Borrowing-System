1fadf008cc56ebf79c56c80a1171423d12880ff3
Gerald Carido <gerald.carido@cit.edu>
2024-03-24 17:35:24 +0800

    Inventory datagridview

--- WindowsFormsApp1/WindowsFormsApp1/StaffForm.cs
+++ WindowsFormsApp1/WindowsFormsApp1/StaffForm.cs
@@ -68,7 +68,7 @@ namespace WindowsFormsApp1
 
         private void staffInventory_Click(object sender, EventArgs e)
         {
-
+            loadform(new StaffInventory());
         }
 
         private void staffTransaction_Click(object sender, EventArgs e)
